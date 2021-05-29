d49d604d02791440230cc919ce1eb2d0eec0d1d7
Kano <takayukikano.0613@gmail.com>
2021-05-29 20:36:43 +0900

    1-5:create table

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->unique(); //-- この行を変更
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable(); //-- この行を変更
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ laravel/database/migrations/2021_05_29_203123_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            //==========ここから追加==========
+            $table->string('title');
+            $table->text('body');
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            //==========ここまで追加==========
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
