118dc1d60047b00f5e09bf263a78680a40b2ee6c
Kano <takayukikano.0613@gmail.com>
2021-05-30 09:55:42 +0900

    1-7:sortByDesc @ ArticleController

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -2,46 +2,55 @@
 
 namespace App\Http\Controllers;
 
+//==========ここから追加==========
+use App\Article;
+//==========ここまで追加==========
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-     public function index()
-     {
-         // ダミーデータ
-         $articles = [
-             (object) [
-                 'id' => 1,
-                 'title' => 'タイトル1',
-                 'body' => '本文1',
-                 'created_at' => now(),
-                 'user' => (object) [
-                     'id' => 1,
-                     'name' => 'ユーザー名1',
-                 ],
-             ],
-             (object) [
-                 'id' => 2,
-                 'title' => 'タイトル2',
-                 'body' => '本文2',
-                 'created_at' => now(),
-                 'user' => (object) [
-                     'id' => 2,
-                     'name' => 'ユーザー名2',
-                 ],
-             ],
-             (object) [
-                 'id' => 3,
-                 'title' => 'タイトル3',
-                 'body' => '本文3',
-                 'created_at' => now(),
-                 'user' => (object) [
-                     'id' => 3,
-                     'name' => 'ユーザー名3',
-                 ],
-             ],
-         ];
- 
-         return view('articles.index', ['articles' => $articles]);
-     }
+
+    public function index()
+    {
+        //==========ここから削除==========
+        // // ダミーデータ
+        // $articles = [
+        //     (object) [
+        //         'id' => 1,
+        //         'title' => 'タイトル1',
+        //         'body' => '本文1',
+        //         'created_at' => now(),
+        //         'user' => (object) [
+        //             'id' => 1,
+        //             'name' => 'ユーザー名1',
+        //         ],
+        //     ],
+        //     (object) [
+        //         'id' => 2,
+        //         'title' => 'タイトル2',
+        //         'body' => '本文2',
+        //         'created_at' => now(),
+        //         'user' => (object) [
+        //             'id' => 2,
+        //             'name' => 'ユーザー名2',
+        //         ],
+        //     ],
+        //     (object) [
+        //         'id' => 3,
+        //         'title' => 'タイトル3',
+        //         'body' => '本文3',
+        //         'created_at' => now(),
+        //         'user' => (object) [
+        //             'id' => 3,
+        //             'name' => 'ユーザー名3',
+        //         ],
+        //     ],
+        // ];
+        //==========ここまで削除==========
+        //==========ここから追加==========
+        $articles = Article::all()->sortByDesc('created_at');
+        //==========ここまで追加==========
+
+        return view('articles.index', ['articles' => $articles]);
+    }
 }
