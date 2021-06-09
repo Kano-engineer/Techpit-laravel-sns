a13bd9d76f2d524779f08c67ab6cd3e8e95341e2
Kano <takayukikano.0613@gmail.com>
2021-06-09 21:33:00 +0900

    3-4〜4-2:add routing

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,5 +11,6 @@
 |
 */
 
-Auth::routes(); //-- この行を追加
-Route::get('/', 'ArticleController@index');
+Auth::routes(); 
+Route::get('/', 'ArticleController@index')->name('articles.index'); //-- この行を編集
+Route::resource('/articles', 'ArticleController')->except(['index']); //-- この行を編集
