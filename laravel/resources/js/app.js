a9d5982d394ce308af97aab19604a6ea0f900f99
Kano <takayukikano.0613@gmail.com>
2021-06-23 15:09:29 +0900

    7-2:Vue.js

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+//==========ここから追加==========
+import './bootstrap'
+import Vue from 'vue'
+import ArticleLike from './components/ArticleLike'
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    ArticleLike,
+  }
+})
+//==========ここまで追加==========
