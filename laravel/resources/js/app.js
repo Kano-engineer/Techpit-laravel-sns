44b4b87b23e18144c01c19c6cc6f3d0ec2943ef1
Kano <takayukikano.0613@gmail.com>
2021-06-25 22:41:11 +0900

    8-2:make ArticleTagsInput.vue

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,10 +1,16 @@
 import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
+//==========ここから追加==========
+import ArticleTagsInput from './components/ArticleTagsInput'
+//==========ここまで追加==========
 
 const app = new Vue({
   el: '#app',
   components: {
     ArticleLike,
+    //==========ここから追加==========
+    ArticleTagsInput,
+    //==========ここまで追加==========
   }
 })
--- laravel/resources/views/articles/form.blade.php
+++ laravel/resources/views/articles/form.blade.php
@@ -3,6 +3,15 @@
   <label>タイトル</label>
   <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}"> {{--この行のvalue属性を変更--}}
 </div>
+
+{{----------ここから追加----------}}
+<div class="form-group">
+  <article-tags-input
+  >
+  </article-tags-input>
+</div>
+{{----------ここまで追加----------}}
+
 <div class="form-group">
   <label></label>
   <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea> {{--この行のtextareaタグ内を編集--}}
